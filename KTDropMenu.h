f33cf34f42d1b79cd8630bb6545358e0ea35a1c5
Jayden Zhao <jaydenzhao@outlook.com>
2015-07-17 18:44:33 +0800

    a drop menu used for IOS

---
+++ KTDropMenu.h
@@ -0,0 +1,47 @@
+
+
+
+
+//
+//  KTDropMenu.h
+//
+//  Created by xingbin
+//  Copyright (c) 2015年 ktplay. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+@interface KTDropMenuItem : NSObject
+
+@property (readwrite, nonatomic, strong) UIImage *image;
+@property (readwrite, nonatomic, strong) NSString *title;
+@property (readwrite, nonatomic, weak) id target;
+@property (readwrite, nonatomic) SEL action;
+@property (readwrite, nonatomic) NSInteger tag;
+
+@property (readwrite, nonatomic, strong) UIColor *foreColor;
+@property (readwrite, nonatomic) NSTextAlignment alignment;
+
++ (instancetype) menuItem:(NSString *) title
+                    image:(UIImage *) image
+                   target:(id)target
+                   action:(SEL) action  tag:(NSInteger)tag;
+
+@end
+
+@interface KTDropMenu : NSObject
+
++ (void) showMenuInView:(UIView *)view
+               fromRect:(CGRect)rect
+              menuItems:(NSArray *)menuItems;
+
++ (void) dismissMenu;
+
++ (UIColor *) tintColor;
++ (void) setTintColor: (UIColor *) tintColor;
+
++ (UIFont *) titleFont;
++ (void) setTitleFont: (UIFont *) titleFont;
+
+@end
